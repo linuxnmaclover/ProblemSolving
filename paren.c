3054ba31c331e3abc8b8b852dbe19a85b10abb06
linuxnmaclover <95326993+linuxnmaclover@users.noreply.github.com>
2021-12-14 23:51:01 -0800

    second version
    After modifications for pattern identification

--- paren.c
+++ paren.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+char **string;
+char *temp=NULL;
 char* generate_parantheses1(int n)
 {
 	int i=0;
 	char *str=(char *)malloc((sizeof(char)*n*2)+1);
-	printf("\nn=%d",n);
 	for (i=0;i<n;i++)
 	{
 		str[i]='(';
@@ -61,30 +61,49 @@ char* generate_parantheses3(int n)
 	return str;
 }
 
+int check_pattern(char *str,int n)
+{
+	int lcount=0;
+	int rcount=0;
+	int i=0;
+	for (i=0;i<strlen(str);i++)
+	{
+		if (str[i]=='(') lcount++;
+		if (str[i]==')') rcount++;
+	}
+	printf("\n");
+	if ((lcount == rcount) && lcount==n) return 1;
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
-	int n,i=0;
-	char **string;
+	int n,i=0,len=0,j=0,len1=0,len2=0;
+	int count=0;
 	printf("\nEnter value for n:");
-	string=(char **)malloc((sizeof(char)*3*n)+1);
 	scanf("%d",&n);
+	string=(char **)malloc((sizeof(char*)*3*n)+1);
 	for (i=n;i>0;i--)
 	{
-		string[n-i]=(char *)malloc((sizeof(char)*2*n)+1);
-		string[n-i]=generate_parantheses1(i);
+		string[count]=(generate_parantheses1(i));
+		count++;
 	}
 	for (i=n;i>0;i--)
 	{
-		string[2*n-i]=(char *)malloc((sizeof(char)*2*n)+1);
-		string[2*n-i]=generate_parantheses2(i);
+		string[count]=(generate_parantheses2(i));
+		count++;
 	}
 	for (i=n;i>0;i--)
 	{
-		string[3*n-i]=(char *)malloc((sizeof(char)*2*n)+1);
-		string[3*n-i]=generate_parantheses3(i);
+		string[count]=(generate_parantheses3(i));
+		count++;
 	}
+	printf("\n");
 	for (i=0;i<3*n;i++)
 	{
-		printf("\nstring[%d]=%s",i,string[i]);
+		if (check_pattern(string[i],n)==1)
+		{
+			printf("\nValid pattern: %s",string[i]);
+		}
 	}
 }
